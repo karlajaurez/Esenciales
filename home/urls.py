4136f5e9458a4a5dbfad786bc92f8dc9b2fbd4b9
Karla Jaurez <karlajaurez@gmail.com>
2016-09-02 19:09:35 -0500

    Vista Productos
    Vista general de productos

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views
-
+from home.views import ProductoView
 # Static files
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,7 +9,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	
+	url(r'^productos$', ProductoView.as_view(), name='productos'),
 ]
+
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from home.models import *
+from django.views.generic import ListView
 
 def index(request):
 	lista_categorias=Categoria.objects.all()
@@ -7,3 +8,12 @@ def index(request):
 	return render(request, 'index.html',{'lista_categorias': lista_categorias, 'slider': slider})
 
 
+# def lista_productos(request):
+# 	lista_productos = Producto.objects.all()
+# 	return render(request, 'productos.html',{'lista_productos': lista_productos})
+
+
+class ProductoView(ListView):
+	template_name = 'productos.html'
+	model = Producto
+	context_object_name = 'lista_productos'
